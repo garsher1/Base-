ffaa6387bf5918d054d150517b44d665832caa57
garsher1 <romir1461@gmail.com>
2026-06-02 18:46:07 +0700

    Add token

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8860367953:AAFsOMt9CyY84nMcycS4Xn3sQbhR1PrrjsI"
+    "8860367953:AAFsOMt9CyY84nMcycS4Xn3sQbhR1PrrjsI",
+    "8614074591:AAEByZGJ8Q-fPo8rECZxL7K7mQkifEwi_08"
   ]
 }
